1f6d363f385ca979eda83a4eb2b37666afbe7951
Rafael Puckoski Josephino <rafael.puckoski.josephino@gmail.com>
2021-09-10 12:51:17 -0300

    Componentes basicos estaticos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,16 @@
+import Navbar from 'components/navbar/index';
+import Footer from 'components/footer/index';
+import DataTable from 'components/dataTable';
 function App() {
   return (
-    <div>
+    <>
+    <Navbar/>
+    <div className = 'container' >
       <h1 className= "text-primary">teste</h1>
+      <DataTable/>
     </div>
+    <Footer/>
+    </>
   );
 }
 
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,10 +4,9 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'assets/css/styles.css';
 import App from './App';
 
-
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App /> 
   </React.StrictMode>,
   document.getElementById('root')
 );
