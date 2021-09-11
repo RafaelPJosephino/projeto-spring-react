ce620e75c141b5efd53574fe52bcbd156a0c1c90
Rafael P Jpsephino <Rafael.puckoski.josephino@gmail.com>
2021-09-11 17:43:34 -0300

    Home, Navigation

--- frontend/src/components/footer/index.tsx
+++ frontend/src/components/footer/index.tsx
@@ -1,13 +1,12 @@
 function Footer() {
     return (
-        <div> <footer className="footer mt-auto py-3 bg-dark">
+        <footer className="footer mt-auto py-3 bg-dark">
             <div className="container">
                 <p className="text-light">App desenvolvido por <a href="https://github.com/RafaelPJosephino" target="_blank" rel="noreferrer">Rafael P Josephino</a></p>
                 <p className="text-light"><small><strong>Semana Spring React</strong><br />
                     Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
             </div>
         </footer>
-        </div>
     );
 }
 
